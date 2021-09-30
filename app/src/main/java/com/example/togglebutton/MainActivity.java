0f4e66b72015dcbb9d255d6469676b07caeff454
VgionTech <vgion.tech@gmail.com>
2021-09-30 23:36:16 +0530

    init

---
+++ app/src/main/java/com/example/togglebutton/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.togglebutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ToggleButton toggleButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toggleButton = findViewById(R.id.toggleButton);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                Toast.makeText(MainActivity.this, "is ON? " + isChecked, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
